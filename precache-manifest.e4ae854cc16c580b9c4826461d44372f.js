074ce809f63148f643f7658c46612458388df39b
Fraser Hamilton <fh21@hw.ac.uk>
2020-01-09 16:04:44 +0000

    Updates

---
+++ precache-manifest.e4ae854cc16c580b9c4826461d44372f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0f3ae8a700c856fc115d0da2adb318",
+    "url": "/instagram-hl/index.html"
+  },
+  {
+    "revision": "b7220fff45e80edfe8c5",
+    "url": "/instagram-hl/static/css/main.4e4627ec.chunk.css"
+  },
+  {
+    "revision": "d710a847f73174b894cc",
+    "url": "/instagram-hl/static/js/2.5706503e.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/instagram-hl/static/js/2.5706503e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7220fff45e80edfe8c5",
+    "url": "/instagram-hl/static/js/main.b25afb57.chunk.js"
+  },
+  {
+    "revision": "104f4d86503e41034c79",
+    "url": "/instagram-hl/static/js/runtime-main.118924c5.js"
+  }
+]);
